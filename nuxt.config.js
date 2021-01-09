283298d2c8171276de2a4af2c8e83ff71ed6a9e0
David Třetina <tretinadavid@gmail.com>
2021-01-09 23:43:19 +0100

    deploy fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,6 @@ export default {
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
     '@nuxtjs/auth-next',
-    "@nuxtjs/vuetify",
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
